7afebd316aa83dba36f6c6910953c53f7b6a1008
ywang282 <ywang282@illinois.edu>
2017-01-12 15:52:19 +0800

    Roll A Ball Added

---
+++ Roll a Ball/Assets/Scripts/PillRotator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PillRotator : MonoBehaviour {
+
+	void Update () {
+		transform.Rotate (new Vector3 (-15, -30, -45) * Time.deltaTime);
+	}
+}
---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 100;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate(){
+		float  moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText();
+		}
+
+		if (other.gameObject.CompareTag ("Stop Pill")) {
+			other.gameObject.SetActive (false);
+			rb.velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+		}
+
+		if (other.gameObject.CompareTag ("Speed Up")) {
+			rb.velocity *= 3;
+		}
+
+		if (other.gameObject.CompareTag ("Warp1")) {
+			rb.position = new Vector3 (-17f, 1f, 17f);
+		}
+
+		if (other.gameObject.CompareTag ("Warp2")) {
+			rb.position = new Vector3 (0f, 0f, 0f);
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12){
+			winText.text = "You Win!";
+		}
+	}
+}  
