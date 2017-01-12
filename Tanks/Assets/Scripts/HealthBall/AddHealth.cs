2eec722789b1918ae9bc115ecddb18b8fe1e5c15
ywang282 <ywang282@illinois.edu>
2017-01-12 15:45:26 +0800

    Tanks added

---
+++ Tanks/Assets/Scripts/HealthBall/AddHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddHealth : MonoBehaviour {
+
+	public float m_HealthAdd = 10;
+		
+	void OnTriggerEnter(Collider other){
+		//Destroy(gameObject);
+		gameObject.SetActive (false);
+		if (other.tag == "Tank") {
+			Rigidbody tank = other.GetComponent<Rigidbody> ();
+			TankHealth targetHealth = tank.GetComponent<TankHealth> ();
+			targetHealth.addHealth (m_HealthAdd);
+		}
+	}
+}
